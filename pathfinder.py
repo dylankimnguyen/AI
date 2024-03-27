489ff7109bd559cf303399c7846703e06f0ea80a
Dylan Nguyen <a1869735@adelaide.edu.au>
2024-03-27 20:08:51 +1030

    peepo

--- pathfinder.py
+++ pathfinder.py
@@ -1,1 +1,12 @@
-#test
+#test\
+function GRAPH-SEARCH (problem, fringe) returns a solution, or failure
+    closed <- an empty set
+    fringe <- INSERT(MAKE-NODE(INITIAL-STATE[problem]), fringe)
+    loop do
+        if fringe is empty then return failure
+        node <- REMOVE-FRONT(fringe)
+        if GOAL-TEST(problem, STATE[node]) then return node
+        if STATE[node] is not in closed then
+            add STATE[node] to closed
+            fringe <- INSERTALL(EXPAND(node, problem), fringe)
+    end
