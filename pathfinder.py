8d36207753133b6a8b44759004619f8e1cc5da92
Dylan Nguyen <a1869735@adelaide.edu.au>
2024-03-27 20:52:03 +1030

    progress

--- pathfinder.py
+++ pathfinder.py
@@ -1,12 +1,34 @@
 #test\
-function GRAPH-SEARCH (problem, fringe) returns a solution, or failure
-    closed <- an empty set
-    fringe <- INSERT(MAKE-NODE(INITIAL-STATE[problem]), fringe)
-    loop do
-        if fringe is empty then return failure
-        node <- REMOVE-FRONT(fringe)
-        if GOAL-TEST(problem, STATE[node]) then return node
-        if STATE[node] is not in closed then
-            add STATE[node] to closed
-            fringe <- INSERTALL(EXPAND(node, problem), fringe)
-    end
+
+def read_map_file(file_path):
+    with open (file_path, 'r') as file:
+        lines = file.readlines()
+
+    map_size = tuple(map(int, lines[0].split()))
+    start_position = tuple(map(int, lines[1].split()))
+    end_position = tuple(map(int, lines[2].split()))
+    map_data = [list(line.split()) for line in lines[3:]]
+
+    return map_size, start_position, end_position, map_data
+
+def graph_search(problem, fringe):
+    closed = set()
+    initial_node = make_node(problem.initial_state)
+    fringe = insert(initial_node, fringe)
+
+    while True: 
+        if is_empty(fringe):
+            return "failure"
+        
+        node =remove_front(fringe)
+
+        if goal_test(problem, state(node)):
+            return node
+        
+        if state(node) not in closed
+            closed.add(state(node))
+            children = expand(node, problem)
+            for child in children:
+                fringe = insert(child, fringe)
+
+        
